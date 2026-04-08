bbbd1b3abbabc31c25ebc98c7ab0473dc87667ab
quean1995 <quean1995@gmail.com>
2026-04-08 14:23:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import BlogList from './pages/BlogList';
 import BlogPost from './pages/BlogPost';
+import Game from './pages/Game';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="blog" element={<BlogList />} />
           <Route path="blog/:id" element={<BlogPost />} />
+          <Route path="game" element={<Game />} />
         </Route>
       </Routes>
     </Router>
